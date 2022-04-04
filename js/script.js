d2729d16e6f0c84d86a961d90bc3fc5af281de7c
akostyanov <akostianov@yandex.ru>
2022-04-04 16:38:01 +0300

    feat: update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-function positiveSum(arr) {
-    return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+function formatDuration(seconds) {
+
+    let year = Math.floor(seconds / 31536000);
+    let day = Math.floor((seconds % 31536000) / 86400);
+    let hour = Math.floor(((seconds % 31536000) % 86400) / 3600);
+    let minute = Math.floor((((seconds % 31536000) % 86400) % 60));
+    let sec = (((seconds % 31536000) % 86400) % 3600) % 0;
+
+
+
+
+
+
+
+    if (years > 1) { return years + ' ' + mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
+    if (years > 0) { return year + ' ' + mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
+
+
+    else if (mounth > 0) { return mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
+
+
+    else if (day > 0) { return day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
+    else if (hour > 0) { return hour + ' ' + minute + ' and ' + sec + ' seconds'; }
+    else if (minute > 0) { return minute + ' and ' + sec + ' seconds'; }
+    else if { return sec + ' seconds' }
+
+
+
+
+
 }
 
-b = [1, 2, 3];
-console.log(positiveSum(b));
+
+console.log(formatDuration(61));
