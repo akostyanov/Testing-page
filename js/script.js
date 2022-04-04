ba4f12350b294fdee1dd2ccfb051a2b2a2f95a46
akostyanov <akostianov@yandex.ru>
2022-04-04 21:37:30 +0300

    feat: update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,48 @@
 function formatDuration(seconds) {
-
+    if (seconds == 0) {return 'now'};
     let year = Math.floor(seconds / 31536000);
     let day = Math.floor((seconds % 31536000) / 86400);
-    let hour = Math.floor(((seconds % 31536000) % 86400) / 3600);
-    let minute = Math.floor((((seconds % 31536000) % 86400) % 60));
-    let sec = (((seconds % 31536000) % 86400) % 3600) % 0;
+    let hour = Math.floor(((seconds % 31536000) % 86400) / 3600);        
+    let minute = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    let sec = Math.floor(((((seconds % 31536000) % 86400) % 3600) % 60) / 1);
+    
+    let Arr = [];  
+
+    if (year > 1) { year = year + ' years';  Arr.push(year);  }
+    else if (year == 1) {year = year + ' year';  Arr.push(year);  }
+    else { year = undefined }
+
+    if (day > 1) { day = day + ' days'; Arr.push(day); }
+    else if (day == 1) {day = day + ' day'; Arr.push(day); }
+    else { day = undefined } 
+
+    if (hour > 1) { hour = hour + ' hours'; Arr.push(hour); }
+    else if (hour == 1) {hour = hour + ' hour'; Arr.push(hour); }
+    else { hour = undefined } 
+    
+    if (minute > 1) { minute = minute + ' minutes'; Arr.push(minute); }
+    else if (minute == 1) {minute = minute + ' minute'; Arr.push(minute); }
+    else { minute = null } 
 
 
+    if (sec > 1) { sec = sec + ' seconds';  Arr.push(sec); }
+    else if (sec == 1) {sec = sec + ' second'; Arr.push(sec); }
+    else { sec =  null } 
 
-
-
-
-
-    if (years > 1) { return years + ' ' + mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
-    if (years > 0) { return year + ' ' + mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
-
-
-    else if (mounth > 0) { return mounth + ' ' + day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
-
-
-    else if (day > 0) { return day + ' ' + hour + ' ' + minute + ' and ' + sec + ' seconds'; }
-    else if (hour > 0) { return hour + ' ' + minute + ' and ' + sec + ' seconds'; }
-    else if (minute > 0) { return minute + ' and ' + sec + ' seconds'; }
-    else if { return sec + ' seconds' }
-
-
-
-
-
+    
+    if (year > 0) { Arr.push(year); }
+    if (day > 0) { Arr.push(day); }
+    if (hour > 0) { Arr.push(hour); }
+    if (minute > 0) { Arr.push(minute); }
+    if (sec > 0) { Arr.push(sec); }
+  
+    
+    var comma = ', ', and = ' and ';
+    if (Arr.length == 1) {return Arr [0]}
+    else return Arr.slice (0, -1).join (comma) + and + Arr.slice (-1);
 }
 
 
-console.log(formatDuration(61));
+console.log(formatDuration(3601))
+
+
